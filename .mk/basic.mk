fcbd9baa25fccdb8e7ac7a52987e335bd62ed17c
Godefroy Ponsinet <gponsinet@pm.me>
2020-11-07 16:58:28 +0100

    chore(basic): add make system variable
    Signed-off-by: Godefroy Ponsinet <gponsinet@pm.me>

--- .mk/basic.mk
+++ .mk/basic.mk
@@ -1,5 +1,17 @@
-ifndef basic.mk
-basic.mk := $(lastword $(MAKEFILE_LIST))
+ifdef OS
+	ifeq ($(OS),Windows_NT)
+		global/system/windows := global/system/windows
+	endif
+endif
+ifndef OS
+	UNAME_S := $(shell uname -s)
+	ifeq ($(UNAME_S),Linux)
+		global/system/linux := global/system/linux
+	endif
+	ifeq ($(UNAME_S),Darwin)
+		global/system/darwin := global/system/darwin
+	endif
+endif
 
 .PHONY: \
 	install \
@@ -27,5 +39,3 @@ prepare:
 release:
 	npx standard-version
 	git push --follow-tags
-
-endif
