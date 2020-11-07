0fd83517cc2e85f124f43eb9a33f4f127a0cdf30
Godefroy Ponsinet <gponsinet@pm.me>
2020-11-07 16:59:40 +0100

    chore: update tsconfig
    Signed-off-by: Godefroy Ponsinet <gponsinet@pm.me>

--- .mk/typescript.mk
+++ .mk/typescript.mk
@@ -1,9 +1,3 @@
-ifndef typescript.mk
-typescript := $(lastword $(MAKEFILE_LIST))
-
-export VOLTA_HOME := $(HOME)/.volta
-export PATH := $(VOLTA_HOME)/bin:$(PATH)
-
 prepare: typescript.prepare
 typescript.prepare:
 	ln -sf eslint_d node_modules/.bin/eslint
@@ -15,5 +9,3 @@ typescript.lint:
 fix: typescript.fix
 typescript.fix:
 	node_modules/.bin/eslint_d fix .
-
-endif
